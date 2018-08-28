9a86fa583f189fe8244ed853b4a4f971c9ab6212
Jacek <jacek205@wp.pl>
2018-08-28 22:12:45 +0200

    equals in collection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -40,5 +40,8 @@ public class Main {
         for (Person person : people) {
             System.out.println(person.getFullName());
         }
+
+        System.out.println(people.contains(kamil));
+        System.out.println(people.contains(youngKamil));
     }
 }
