e9b268c671fc7540a0cb5ccbfc8e767ffae40be3
Jacek <jacek205@wp.pl>
2018-08-28 21:57:16 +0200

    person + student classess

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Test");
+
+        Person kamil = new Person("kamil", "parzych", 32);
+
+        System.out.println(kamil);
+
+        Person jacek = new Person("jacek", "popiołek", 36);
+
+        System.out.println(jacek);
+
+        System.out.println(kamil.equals(jacek));
+
+        Person youngKamil = new Person("kamil", "parzych", 16);
+
+        System.out.println(kamil.equals(youngKamil));
+
+        Student jan = new Student("jan", "kowalski", 22, "123456");
+
+        System.out.println(jan.equals(jacek));
     }
 }
---
+++ src/com/company/Student.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Student extends Person {
+
+    private String indexNo;
+
+    public Student(String firstName, String lastName, int age, String indexNo) {
+        super(firstName, lastName, age);
+        this.indexNo = indexNo;
+    }
+
+
+    public String getIndexNo() {
+        return indexNo;
+    }
+
+    public void setIndexNo(String indexNo) {
+        this.indexNo = indexNo;
+    }
+}
